917e26b4992d94254b7c3d1c315ea791d828d75b
saurabh <saurabhverma2698@gmail.com>
2023-10-25 17:20:13 +0530

    first Commit

---
+++ assests/js/custom.js
@@ -0,0 +1,8 @@
+$('.slick-carousel').slick({
+    infinite: true,
+    slidesToShow: 1, // Shows a three slides at a time
+    slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
+    dots: true, // Adds the dots on the bottom
+    autoplay:true,
+    speed:200
+  });
